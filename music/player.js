ac4a7d92ddf7a003f01e478e5a122a827552545a
2-4-2 <68284149+2-4-2@users.noreply.github.com>
2026-03-25 21:52:01 +0300

    player.js

--- music/player.js
+++ music/player.js
@@ -1,5 +1,5 @@
 const { createAudioPlayer, createAudioResource, AudioPlayerStatus } = require("@discordjs/voice");
-const play = require("play-dl");
+const ytdl = require("ytdl-core");
 const prism = require("prism-media");
 
 const queues = new Map();
@@ -14,15 +14,14 @@ async function playSong(id) {
   if (!q || !q.songs.length) { q?.connection?.destroy(); queues.delete(id); return; }
 
   const song = q.songs[0];
-  const stream = await play.stream(song.url);
-  let audioStream = stream.stream;
+  let stream = ytdl(song.url, { filter: 'audioonly', highWaterMark: 1<<25 });
 
   if (q.bass) {
     const ffmpegProcess = new prism.FFmpeg({ args:["-analyzeduration","0","-loglevel","0","-i","pipe:0","-af","bass=g=15","-f","s16le","-ar","48000","-ac","2"] });
-    audioStream = audioStream.pipe(ffmpegProcess);
+    stream = stream.pipe(ffmpegProcess);
   }
 
-  const resource = createAudioResource(audioStream, { inputType: stream.type });
+  const resource = createAudioResource(stream);
   q.player.play(resource);
   q.player.once(AudioPlayerStatus.Idle, () => { q.songs.shift(); playSong(id); });
 }
